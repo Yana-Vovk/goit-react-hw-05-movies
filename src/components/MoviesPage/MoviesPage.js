56601ea9f92770395a7ddd3225f99aa854368438
Yana Vovk <yanavovk1990@outlook.com>
2021-10-11 23:55:33 +0300

    Fix search options

--- src/components/MoviesPage/MoviesPage.js
+++ src/components/MoviesPage/MoviesPage.js
@@ -1,16 +1,30 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useLocation, useHistory } from 'react-router-dom';
 // import PropTypes from 'prop-types';
 import styles from './MoviesPage.Module.css';
+import {getSearchMovies} from '../../services/movies-api.js';
 
 export default function MoviesPage({ onSearch }) {
   const [search, setSearch] = useState('');
   const location = useLocation();
   const history = useHistory();
-  const [query, setQuery] = useState(() => {
-    return new URLSearchParams(location.search).get('query') ?? '';
+  const [query, setQuery] = useState('');
+  const [movies, setMovies] = useState([]);
+
+  // const [query, setQuery] = useState(() => {
+  //   return new URLSearchParams(location.search).get('query') ?? '';
+  // });
+  useEffect(()=>{
+    const searchQuery = new URLSearchParams(location.search).get('query');
+    
+    if (!searchQuery) return;
+
+  getSearchMovies(searchQuery).then(data => {
+    setMovies([...data.data.results]);
   });
-  
+  }, [location.search]);
+
+ 
   const handleChange = event => {
     setSearch(event.currentTarget.value);
   };
